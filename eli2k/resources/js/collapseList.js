c9b0ce74ffb28731977c45d8d06f50c4d5707fe6
Eli2k765 <erodgers@eli2k.com>
2023-05-22 14:57:21 -0400

    List Issues

--- eli2k/resources/js/collapseList.js
+++ eli2k/resources/js/collapseList.js
@@ -1,38 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // Check if the page contains the required element
+  const pageContent = document.querySelector('div.page-content.blog.border');
+  if (!pageContent) {
+    return; // Exit the script if the element is not found
+  }
 
-  // Get all the <h3>elements on the page
-  const headings = document.querySelectorAll('div.page-content.blog h3, div.page-content.blog h4, div.page-content.blog h5, div.page-content.blog h6');
+  // Get all the header elements (h3, h4, h5, h6) within the page content
+  const headers = pageContent.querySelectorAll('h3, h4, h5, h6');
 
-  // Loop through each <h3> element
-  headings.forEach((heading) => {
-    // Check if the heading has any child elements
-    const childElements = Array.from(heading.nextElementSibling.children);
+  // Loop through each header element
+  headers.forEach((header) => {
+    // Get the parent div of the header
+    const parentDiv = header.nextElementSibling;
+
+    // Check if the parent div has any child elements
+    const childElements = Array.from(parentDiv.children);
     if (childElements.length) {
       // Hide the child elements by default
       childElements.forEach((child) => {
         child.classList.add('hidden');
       });
 
-      // Add a click event listener to the <h3> element
-      heading.addEventListener('click', () => {
+      // Add a click event listener to the header element
+      header.addEventListener('click', () => {
         // Toggle the display of the child elements
         childElements.forEach((child) => {
           child.classList.toggle('hidden');
         });
 
-        // Toggle the icon and class on the heading element
-        heading.classList.toggle('expanded');
-        const iconElement = heading.querySelector('.icon');
+        // Toggle the icon and class on the header element
+        header.classList.toggle('expanded');
+        const iconElement = header.querySelector('.icon');
         if (iconElement) {
           iconElement.innerHTML = (iconElement.innerHTML === '+') ? '-' : '+';
         }
       });
 
-      // Add a plus or minus icon to the <h3> element
+      // Add a plus or minus icon to the header element
       const iconElement = document.createElement('span');
       iconElement.classList.add('icon');
       iconElement.innerHTML = '+';
-      heading.insertBefore(iconElement, heading.firstChild);
+      header.insertBefore(iconElement, header.firstChild);
     }
   });
 
@@ -44,30 +52,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Add a click event listener to the toggle button
   toggleButton.addEventListener('click', () => {
-    // Toggle the display of all child elements
-    headings.forEach((heading) => {
-      const childElements = Array.from(heading.nextElementSibling.children);
-      childElements.forEach((child) => {
-        child.classList.toggle('hidden');
-      });
+    const shouldExpand = toggleButton.classList.contains('show-all');
 
-      // Toggle the icon and class on the heading element
-      heading.classList.toggle('expanded');
-      const iconElement = heading.querySelector('.icon');
-      if (iconElement) {
-        iconElement.innerHTML = (iconElement.innerHTML === '+') ? '-' : '+';
+    headers.forEach((header) => {
+      const parentDiv = header.nextElementSibling;
+      const childElements = Array.from(parentDiv.children);
+
+      if (shouldExpand) {
+        childElements.forEach((child) => {
+          child.classList.remove('hidden');
+        });
+        header.classList.add('expanded');
+        const iconElement = header.querySelector('.icon');
+        if (iconElement) {
+          iconElement.innerHTML = '-';
+        }
+      } else {
+        childElements.forEach((child) => {
+          child.classList.add('hidden');
+        });
+        header.classList.remove('expanded');
+        const iconElement = header.querySelector('.icon');
+        if (iconElement) {
+          iconElement.innerHTML = '+';
+        }
       }
     });
 
-    // Toggle the class and text on button
     toggleButton.classList.toggle('show-all');
     toggleButton.classList.toggle('hide-all');
-    
-    // Check the current class on button and update the text accordingly
-    if (toggleButton.classList.contains('show-all')) {
-      toggleButton.innerHTML = 'Show All';
-    } else {
-      toggleButton.innerHTML = 'Hide All';
-    }
+    toggleButton.innerHTML = shouldExpand ? 'Hide All' : 'Show All';
   });
 });
